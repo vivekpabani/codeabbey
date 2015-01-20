ed6bfa46f4e7c727730244702bf83717d2a83720
vivekpabani <viky.pabani@gmail.com>
2015-01-20 08:33:14 -0600

    First Update

---
+++ 006/temp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+"""
+Problem Definition :
+
+
+"""
+
+__author__ = 'vivek'
+
+import time
+
+startTime = time.clock()
+
+print int(round(5/3))
+
+print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
