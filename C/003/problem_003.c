56a10cac3bef803f2618b20274c8c4c4a139f548
Vivek Pabani <vivekpabani@users.noreply.github.com>
2015-05-28 15:56:35 +0200

    Update problem_003.c

--- C/003/problem_003.c
+++ C/003/problem_003.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*
+Problem Definition -
+Now you will be given several pairs of values and you need to calculate sum for each pair.
+Input data will contain the total count of pairs to process in the first line.
+The following lines will contain pairs themselves - one pair at each line.
+Answer should contain the results separated by spaces.
+*/
+
 int main(void){
 
   return 1;
