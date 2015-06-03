d7b43c045801458fc5a0d2a9018ef8d35a0c82d4
Vivek Pabani <vivekpabani@users.noreply.github.com>
2015-06-03 10:03:05 +0200

    Update problem_003.c

--- C/003/problem_003.c
+++ C/003/problem_003.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <math.h>
 /*
 Problem Definition -
 Now you will be given several pairs of values and you need to calculate sum for each pair.
@@ -8,12 +8,22 @@ The following lines will contain pairs themselves - one pair at each line.
 Answer should contain the results separated by spaces.
 */
 
-int main(void){
-  
+int main(int argc, char *argv[]){
+  int count=0, i, start=2, sum=0;  
   /* Take Count */
-
-  /* loop over pairs*/
+  if ( argc != 2 ) /* argc should be 2 for correct execution */ 
+  {
+    count = atoi(argv[1]);
+  }
   
+  /* loop over pairs*/
+  for(i=0; i<count; ++i)
+  {
+    sum = atoi(argv[start]) + atoi(argv[start+1]);
+    printf("%d\n",sum);
+    start+=2;
+  }
+}
   /* sum of ech pair*/
   return 1;
 }
